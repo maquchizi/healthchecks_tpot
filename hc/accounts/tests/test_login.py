239eda6a6a8420487943c5b674748a6bb2e6fc48
Mark Ng'ang'a <maqnganga@gmail.com>
2016-12-07 20:29:03 +0300

    Fix LoginTestCase
     * Assert that a user was created
     * Assert contents of the email body
     * Assert that check is associated with the new user

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -19,19 +19,26 @@ class LoginTestCase(TestCase):
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
 
-        ### Assert that a user was created
+        # Assert that a user was created
+        user = User.objects.get(email="alice@example.org")
+        self.assertEqual("alice@example.org", user.email)
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        ### Assert contents of the email body
 
-        ### Assert that check is associated with the new user
+        # Assert contents of the email body
+        self.assertIn(
+            'To log into healthchecks.io, please open the link below:',
+            mail.outbox[0].body)
+
+        # Assert that check is associated with the new user
+        check = Check.objects.get(user_id=user.id)
+        self.assertEqual(check.user_id, user.id)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
         self.client.get("/accounts/login/")
         assert "bad_link" not in self.client.session
 
-        ### Any other tests?
-
+        # Any other tests?
