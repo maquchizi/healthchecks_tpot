6719e5c4d48c061fd953794ad31108884bb734e5
Mark Ng'ang'a <maqnganga@gmail.com>
2016-12-08 12:37:26 +0300

    Fix test_check_token

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -22,7 +22,15 @@ class CheckTokenTestCase(BaseTestCase):
         self.assertEqual(self.profile.token, "")
 
     ### Login and test it redirects already logged in
+    def test_redirects_already_logged_id(self):
+        self.client.login(username="alice@example.org", password="password")
+        r = self.client.post("/accounts/check_token/alice/secret-token/")
+        self.assertRedirects(r, "/checks/")
+
 
     ### Login with a bad token and check that it redirects
+    def test_redirects_bad_token(self):
+        r = self.client.post("/accounts/check_token/alice/some-token/")
+        self.assertRedirects(r, "/accounts/login/")
 
     ### Any other tests?
